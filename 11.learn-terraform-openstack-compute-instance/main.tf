be22740eee7c2686eb40f214aba0009f8f4b16c8
yongs2 <yongs2@hotmail.com>
2023-03-14 14:50:23 +0900

    fix: add openstack_compute_flavor_v2

--- 11.learn-terraform-openstack-compute-instance/main.tf
+++ 11.learn-terraform-openstack-compute-instance/main.tf
@@ -1,5 +1,14 @@
 provider "openstack" {}
 
+# openstack flavor list; openstack flavor show C1M2D20
+resource "openstack_compute_flavor_v2" "C1M2D20" {
+  name  = "C1M2D20"
+  ram   = "2048"
+  vcpus = "1"
+  disk  = "20"
+  swap  = "4096"
+}
+
 # openstack keypair list; openstack keypair show basic
 resource "openstack_compute_keypair_v2" "basic" {
   name       = "basic"
@@ -8,9 +17,9 @@ resource "openstack_compute_keypair_v2" "basic" {
 
 # openstack server list; openstack server show basic
 resource "openstack_compute_instance_v2" "basic" {
-  name            = "basic"
-  image_name      = var.image
-  flavor_name     = var.flavor
+  name       = "basic"
+  image_name = var.image
+  flavor_id       = openstack_compute_flavor_v2.C1M2D20.id
   key_pair        = openstack_compute_keypair_v2.basic.name
   security_groups = ["default"]
 
